f74b1f0cc798706201585121d5cc362ba78323f6
Alexander Mould <alexmould@hotmail.co.uk>
2024-10-10 18:07:29 +0100

    Adding model definitions

--- quotava/quotes/models.py
+++ quotava/quotes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    is_admin = models.BooleanField(default=False)
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    birth_date = models.DateField(null=True, blank=True)
+    death_date = models.DateField(null=True, blank=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Quote(models.Model):
+    text = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
